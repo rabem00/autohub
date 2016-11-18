15a0f8d9f92357ebf68e5761398e35de66fa8716
M Rabelink <marco.rabelink@icloud.com>
2016-11-18 18:37:48 +0100

    working

--- app/hubupload/views.py
+++ app/hubupload/views.py
@@ -8,6 +8,7 @@ from flask import render_template
 from flask import abort
 from flask import jsonify
 from flask import current_app
+from flask import redirect
 from flask.views import MethodView
 from app.base import ApiResource
 
@@ -35,3 +36,19 @@ class HubResourceUpload(ApiResource):
             return jsonify(input_status)
         else:
             return render_template('upload.html', data=input_status, config=self.config)
+
+    def post(post):
+        name=request.form['yourname']
+        email=request.form['youremail']
+        selecttype=request.form['selecttype']
+        textlist=request.form['textlist']
+        inputfile=request.form['inputfile']
+
+        print name
+        print email
+        print selecttype
+        print textlist
+        print inputfile
+
+
+        return redirect('/')
