444d1bd9b89786dff6c753a5cd98355a9b2232ba
M Rabelink <marco.rabelink@icloud.com>
2016-11-20 19:23:56 +0100

    New layout

--- app/hub/views.py
+++ app/hub/views.py
@@ -29,15 +29,9 @@ class HubResource(ApiResource):
         self.config = current_app.config['USER_CONFIG']
 
     def get(self):
-        if not (request.args.get('id') == None):
-            id = request.args.get('id')
-            with open('./testdata.json') as data_file:
-                input_status = json.load(data_file)
-            return render_template('details.html', data=input_status, id=id, config=self.config)
+        with open('./testdata.json') as data_file:
+            input_status = json.load(data_file)
+        if request.mimetype == 'application/json':
+            return jsonify(input_status)
         else:
-            with open('./testdata.json') as data_file:
-                input_status = json.load(data_file)
-            if request.mimetype == 'application/json':
-                return jsonify(input_status)
-            else:
-                return render_template('latest.html', data=input_status, config=self.config)
+            return render_template('latest.html', data=input_status, config=self.config)
